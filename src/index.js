240d7af9b11b02b81b06f07db1f904659cba2c99
kevinroan <kevinnroan@gmail.com>
2021-04-01 04:47:47 -0400

    make textpanel

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ window.addEventListener("load", () => {
 
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
-  camera.position.z = 1;
+  camera.position.z = 3;
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -23,54 +23,82 @@ window.addEventListener("load", () => {
   animate();
 });
 
-class Scene extends {
+class Scene extends Object3D {
+  // const room = new THREE.LineSegments(
+  //   new BoxLineGeometry(6, 6, 6, 10, 10, 10).translate(0, 3, 0),
+  //   new THREE.LineBasicMaterial({ color: 0x808080 })
+  // );
 
+  constructor(){
+
+    this.add()
+  }
 }
 
 class ScenePanel extends Object3D {
-  constructor(){
+  constructor(title, description, position){
     super();
+
+    this.init('Scene Title', 'Scene Description');
   }
 
   init(title, description){
+    const container = new ThreeMeshUI.Block({
+      width: 1,
+      height: 0.5
+    });
+  
+    container.position.set(0, 0, 0);
+    // container.rotation.x = -0.3;
+    this.add(container);
 
+    const textBlock = new ThreeMeshUI.Block({
+      height: 0.1,
+      width: 0.9,
+      margin: 0.01,
+      padding: 0.02,
+      fontSize: 0.025,
+      alignContent: "left",
+      backgroundColor: new THREE.Color( 'blue' ),
+      backgroundOpacity: 0.0,
+
+      fontFamily:
+        "https://unpkg.com/three-mesh-ui/examples/assets/Roboto-msdf.json",
+      fontTexture:
+        "https://unpkg.com/three-mesh-ui/examples/assets/Roboto-msdf.png"
+    }).add(
+      new ThreeMeshUI.Text({
+        content: title + '\n',
+        fontSize: 0.05,
+        // fontColor: new THREE.Color(0x96ffba)
+      }),  
+      new ThreeMeshUI.Text({
+        content: description
+      })
+    );
+
+    textBlock.position.set(0, -0.13, 0.04);
+    // textBlock.rotation.x = -0.3;
+
+    this.add(textBlock);
   }
 }
 
 function makeTextPanel() {
-  const container = new ThreeMeshUI.Block({
-    width: 1.2,
-    height: 0.5,
-    padding: 0.05,
-    justifyContent: "center",
-    alignContent: "left",
-    fontFamily:
-      "https://unpkg.com/three-mesh-ui/examples/assets/Roboto-msdf.json",
-    fontTexture:
-      "https://unpkg.com/three-mesh-ui/examples/assets/Roboto-msdf.png"
-  });
 
-  container.position.set(0, 0, 0);
-  container.rotation.x = -0.3;
-  scene.add(container);
+  for(var i = 0;i<3;i++){
+    for(var j=0;j<3;j++){
 
-  //
+      var x = 1.1*i-1;
+      var y = j*0.6;
 
-  container.add(
-    new ThreeMeshUI.Text({
-      content: "This library supports line-break-friendly-characters,",
-      fontSize: 0.055
-    }),
-
-    new ThreeMeshUI.Text({
-      content:
-        " As well as multi-font-size lines with consistent vertical spacing.",
-      fontSize: 0.08
-    })
-  );
+      let panel = new ScenePanel();
+      panel.position.set(x, y, 0);
+    
+      scene.add(panel);
+    }
+  }
 }
-
-//
 
 const animate = function () {
   requestAnimationFrame(animate);
